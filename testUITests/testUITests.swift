01241423bd25d430120f1de72119a4b1d39902a3
Anatoly Cherenkov <anatoly.cherenkov@gmail.com>
2024-07-26 15:43:57 +0200

    add test output

--- testUITests/testUITests.swift
+++ testUITests/testUITests.swift
@@ -19,6 +19,7 @@ class testUITests: XCTestCase {
 
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         app.launch()
+        print("Application has been launched.")
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
